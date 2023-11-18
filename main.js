2c147ed8aa4636facb164b0e6f22eb003aa43c16
natanbinisti <natanbinistilyon@gmail.com>
2023-11-19 00:47:03 +0100

    exercice pour la mise en bouche

---
+++ main.js
@@ -0,0 +1,22 @@
+//exo2
+let coucou = prompt("salut");
+
+
+//exo 2
+let a = 10
+let b = 5
+let result = a + b
+console.log(result)
+
+
+
+//exo3
+let note_maths = 11;
+let note_francais = 9;
+let note_hg = 16;
+let moyenne = (note_francais+note_maths+note_hg) / 3;
+
+alert('La moyenne est de ' + moyenne);
+
+
+//exo4
